2e25b3e0bbf4ec910068df91adb029121451a65d
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-05-30 05:46:31 +0700

    Add files via upload

---
+++ mbf.sh
@@ -0,0 +1,11 @@
+figlet -f future "Acep Gans"
+echo "my github : github.com/aceptriana"
+echo ""
+echo " sedang mengintall bahan"
+echo ""
+sleep 1
+pkg install python2 git
+pip2 install mechanize
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 jomblo.py
